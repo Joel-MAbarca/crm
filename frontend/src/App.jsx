59ca22b3dc23c37f8fe0d8ea865ca600f861fe11
Joel-MAbarca <j.e.morales.abarca@gmail.com>
2025-10-26 22:33:57 -0300

    front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="crm-container">
       <h1>CRM Dashboard</h1>
       <p>API URL: {import.meta.env.VITE_API_URL}</p>
-      <ClientList /> 
+      <Login /> 
     </div>
   );
 }
