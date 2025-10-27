712f3aeb0d338a4390cb2cd27516f47518bc4b70
Joel-MAbarca <j.e.morales.abarca@gmail.com>
2025-10-26 22:23:58 -0300

    front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css'; 
-import ClientList from './components/ClientList'; 
+import Login from './components/auth/Login';
 
 function App() {
   return (
